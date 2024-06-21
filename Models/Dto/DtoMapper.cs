48dc7556904c94aa8eca51f3ef3bf7f14c9c22cb
Mounir Komenan <s1188188@student.windesheim.nl>
2024-06-21 16:16:19 +0200

    priceperday added to week 6

--- Models/Dto/DtoMapper.cs
+++ Models/Dto/DtoMapper.cs
@@ -10,7 +10,8 @@ namespace CadvancedOpdracht.Models.Dto
                 .ForMember(dest => dest.imageURL, opt => opt.MapFrom(src =>
                     src.Images.FirstOrDefault(i => i.IsCover).Url))
                 .ForMember(dest => dest.landlordAvatarURL, opt => opt.MapFrom(src =>
-                    src.Landlord.Avatar.Url));
+                    src.Landlord.Avatar.Url))
+            .ForMember(dest => dest.price, opt => opt.MapFrom(src => src.PricePerDay));
 
             CreateMap<Image, ImageDto>();
             CreateMap<Landlord, LandlordDto>();
--- Models/Dto/LocationDto.cs
+++ Models/Dto/LocationDto.cs
@@ -7,5 +7,6 @@
         public string description { get; set; }
         public string imageURL { get; set; }
         public string landlordAvatarURL { get; set; }
+        public float price { get; set; }
     }
 }
